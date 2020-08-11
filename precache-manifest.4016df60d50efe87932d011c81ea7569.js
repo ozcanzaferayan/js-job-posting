f5ca2763eaba3473d26cf3d5b56025bdb69c30da
ozcanzaferayan <ozcanzaferayan@users.noreply.github.com>
2020-08-11 19:04:04 +0000

    deploy: 8eaa953b3da3b2e13e859cc758df490e97389bf6

---
+++ precache-manifest.4016df60d50efe87932d011c81ea7569.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781ce7c7408fa699a27e87d850a57425",
+    "url": "/js-job-posting/index.html"
+  },
+  {
+    "revision": "9d7cdcca1c2430efe602",
+    "url": "/js-job-posting/static/css/main.1fa854ac.chunk.css"
+  },
+  {
+    "revision": "1377b2e52dbf7eadd9f9",
+    "url": "/js-job-posting/static/js/2.d73b5c88.chunk.js"
+  },
+  {
+    "revision": "ecf565b120ac37fb6bafc33c549ab184",
+    "url": "/js-job-posting/static/js/2.d73b5c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7cdcca1c2430efe602",
+    "url": "/js-job-posting/static/js/main.f8f5f263.chunk.js"
+  },
+  {
+    "revision": "5cc86e8568249343ec40",
+    "url": "/js-job-posting/static/js/runtime-main.14f4b921.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/js-job-posting/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1109a6d4445cfca24d9a6441e51ddcd6",
+    "url": "/js-job-posting/static/media/react.1109a6d4.png"
+  },
+  {
+    "revision": "32a1e27df2bdb6d35cc5867c273b5e2b",
+    "url": "/js-job-posting/static/media/vue.32a1e27d.png"
+  }
+]);
